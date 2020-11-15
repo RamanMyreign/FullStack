a7fafe50a17b39bd5ac024882cd36abc63a76716
RamanMyreign <raman.shukla_cs18@gla.ac.in>
2020-11-15 20:20:14 +0530

    selectors in js file are created

---
+++ prcatical17/practical17.js
@@ -0,0 +1,4 @@
+const time = document.getElementById('time');
+const greeting = document.getElementById('greeting');
+const name = document.getElementById('name');
+const date = document.getElementById('date');

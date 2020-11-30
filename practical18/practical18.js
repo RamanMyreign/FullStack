2b25c6e2b7a6dc3682c66e955a65d010fc33aced
RamanMyreign <raman.shukla_cs18@gla.ac.in>
2020-11-30 17:23:09 +0530

    email valid function is created

--- practical18/practical18.js
+++ practical18/practical18.js
@@ -16,30 +16,37 @@ form.addEventListener('submit', function(e){
 
 function checkInput(){
     if(username.value === ''){
-        showError(username, "Username required");
+        showError(username, "Username can't be blank");
     }
     else{
         showSuccess(username);
     }
     if(email.value === ''){
-        showError(email, "Username required");
+        showError(email, "Email field can't be blank");
+    }
+    else if(!isEmailValid(email.value)){
+        showError(email,"Email is not valid")
     }
     else{
-        showSuccess(username);
+        showSuccess(email);
     }
     if(password1.value === ''){
-        showError(password1, "Username required");
+        showError(password1, "Password field can't be empty");
     }
     else{
-        showSuccess(username);
+        showSuccess(password1);
     }
     if(password2.value === ''){
-        showError(password2, "Username required");
+        showError(password2, "this field can't be empty");
+    }
+    else if(password1.value !== password2.value ){
+        showError(password2, "Password is not same as previous");
     }
     else{
-        showSuccess(username);
+        showSuccess(password2);
     }
 }
+
 
 function showError(input, msg){
   const form_div = input.parentNode;
@@ -53,5 +60,10 @@ function showSuccess(input){
     const form_div = input.parentNode;
   form_div.className = 'form-div success';
   const small = form_div.querySelector('small');
+  small.innerHTML = '';
 
+}
+
+function isEmailValid(email){
+    return /^([a-zA-Z0-9_\.\-]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,3})$/.test(email);
 }
